b815a2deb86931d3d43d58f55c9f5fdade65a322
Joshua Rosenberg <jrosen@msu.edu>
2016-11-26 15:56:52 -0500

    final changes

--- analysis.R
+++ analysis.R
@@ -18,7 +18,7 @@ for (i in 1:length(list.files("data"))){
 list.files("data")
 
 names(data_list) <- c("Illinois", "Indiana", "Northwestern", "Ohio State", "Penn State",
-                      "Purdue", "Rutgers", "Spartans", "Iowa", "Michigan", "Maryland",
+                      "Purdue", "Rutgers", "Michigan State", "Iowa", "Michigan", "Maryland",
                       "Nebraska", "Minnesota", "Wisconsin")
 
 data <- plyr::ldply(data_list)
@@ -28,40 +28,68 @@ str(data)
 data$date <- lubridate::ymd_hms(data$status_published)
 data$year <- lubridate::year(data$date)
 data$month <- lubridate::month(data$date)
+data$month
 data$printed_date <- as.numeric(paste0(data$year, ".", data$month))
+
+data$printed_date
 
 str(data)
 
-data_ss <- data %>%
-    filter(year > 2010) %>% 
-    group_by(printed_date, .id) %>%
-    summarize(num = sum(num_reactions))
+data_ss <-
+    data %>%
+        filter(year > 2010) %>% 
+        group_by(date, .id) %>%
+        summarize(num = sum(num_reactions))
+
+str(data_ss)
 
 colorCount = 14
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 getPalette(colorCount)
+the_palette <- getPalette(colorCount)
 
-length(unique(data_ss$printed_date))
+# psu #002469
+# illinois: #e87722
+# indiana: #9d2235
+# iowa: #ffcd00
+# msu: #173f35
+# minn: #FFBF3F
+# neb: #c8102e
+# nw: #5f259f
+# osu: #a2aaad
+# pursue: #85714d
+# wisc: #ba0c2f
+# mich: #00274c
+# rut: #000000
+# maryland: #ed1c24
+
+the_palette <- c("#e87722", "#9d2235", "#ffcd00", "#ed1c24", 
+                 "#00274c", "#FFBF3F", "#c8102e", "#5f259f",
+                 "#a2aaad", "#002469", "#85714d", "#000000",
+                 "#173f35", "#ba0c2f")
+
+the_palette <- tolower(the_palette)
+sort(unique(data_ss$printed_date))
 
 72/12
 
 str(data_ss)
 
-ggplot(data_ss, aes(x = printed_date, y = num, color = .id, group = .id)) +
+?scale_x_discrete
+
+ggplot(data_ss, aes(x = date, y = num, color = .id, group = .id)) +
     geom_line() +
-    geom_point() +
     facet_wrap( ~ .id) +
     scale_y_continuous(labels = scales::comma) +
-    scale_fill_manual(values = getPalette(colorCount)) +
-    scale_x_discrete(breaks = 1:6, labels = c("2011", "2012", "2013", "2014", "2015", "2016")) +
+    scale_color_manual(values = the_palette) +
     theme_minimal() +
-    theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
     theme(axis.text.y = element_text(angle = 45, hjust = 1)) +
     ylab(NULL) +
     xlab(NULL) +
     theme(text =element_text(size = 15, family = "Georgia")) +
-    ggtitle("Interactions with B1G Facebook Pages (2011 - 2016)") +
-    theme(legend.position = "bottom") +
-    labs(color = NULL)
+    ggtitle("Reactions to B1G Facebook Page Posts (2011 - 2016)") +
+    theme(legend.position = "none") +
+    labs(caption = "Data collected on 11-26-2016. More information is at https://github.com/jrosen48/scrape_b1g_facebook_pages.")
 
 ggsave("b1gpages.png")
